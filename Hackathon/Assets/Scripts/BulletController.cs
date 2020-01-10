0b995752200bac9cfdd3e375b045b3a440b53c33
Dario van den Boom <dario.van-den-boom@stud.uni-due.de>
2020-01-10 19:19:49 +0100

    Added: Guns and bullets

---
+++ Hackathon/Assets/Scripts/BulletController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+
+    public Vector2 direction;
+    public float speed = 1;
+
+    public float lifetime = 10;
+
+    void Start()
+    {
+        direction = Vector3.Normalize(direction);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        lifetime -= Time.fixedDeltaTime;
+
+        if (lifetime <= 0)
+        {
+            Destroy(gameObject);
+        }
+
+        rb.MovePosition(rb.position + direction * speed * Time.fixedDeltaTime);
+    }
+}
---
+++ Hackathon/Assets/Scripts/GunController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunController : MonoBehaviour
+{
+    public GameObject bulletPrefab;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log(Input.mousePosition);
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 20;
+
+            Vector3 mouseWorldPos = Camera.main.ScreenToWorldPoint(mousePos);
+            Vector3 direction = Vector3.Normalize(mouseWorldPos - transform.position);
+            Debug.Log(direction.magnitude);
+
+            GameObject bullet = Instantiate(bulletPrefab, transform.position, Quaternion.identity);
+            bullet.transform.localScale = 0.5f * bullet.transform.localScale;
+            BulletController bulletController = bullet.GetComponent<BulletController>();
+            bulletController.direction = direction;
+        }
+    }
+}
